b3236bb2bef4a9923784d934c7b01bf89e8f557c
Tony Brown <tony@tonybrown.me>
2022-06-15 13:29:45 -0400

    able to log user in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,20 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route          /api/users/login
 //@access         Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('login route')
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error(`Invalid credentials`)
+  }
 })
 
 module.exports = {
