e5257989b302c448ad990d397539601602e3c083
Tony Brown <tony@tonybrown.me>
2022-06-15 12:26:00 -0400

    set up password encryption

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 8000
 
+// Connect to database
+connectDB()
+
 const app = express()
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Welcome to the support desk api' })
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
