1e51913de47803a69f34e3d5faffde171a965e87
Tony Brown <tony@tonybrown.me>
2022-06-16 09:19:30 -0400

    add new ticket page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,11 @@ import {
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute.js'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import NewTicket from './pages/newTicket'
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
